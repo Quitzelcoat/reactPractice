9701fda1a2511aadcb6579dc43e0add970b982e0
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-06-29 16:07:08 +0500

    react example

--- my-first-react-app/src/Age.jsx
+++ my-first-react-app/src/Age.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 
 export default function Person() {
-  const [person, setPerson] = useState({ name: "John", age: 100 });
+  const [person, setPerson] = useState({ name: "", age: 100 });
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {
+    const newFirstName = e.target.value;
+    setFirstName(newFirstName);
+    setPerson({ ...person, name: newFirstName + " " + lastName });
+  }
+
+  function handleLastNameChange(e) {
+    const newLastName = e.target.value;
+    setLastName(newLastName);
+    setPerson({ ...person, name: firstName + " " + newLastName });
+  }
 
   const handleIncreaseAge = () => {
     console.log("in handleIncreaseAge (before setPerson call): ", person);
     setPerson({ ...person, age: person.age + 1 });
-    // we've called setPerson, surely person has updated?
     console.log("in handleIncreaseAge (after setPerson call): ", person);
   };
 
-  // this console.log runs every time the component renders
-  // what do you think this will print?
   console.log("during render: ", person);
 
   return (
     <>
+      <label>
+        First Name: <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last Name: <input value={lastName} onChange={handleLastNameChange} />
+      </label>
       <h1>{person.name}</h1>
       <h2>{person.age}</h2>
       <button onClick={handleIncreaseAge}>Increase age</button>
