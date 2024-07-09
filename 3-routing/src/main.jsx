0bdb7cea052721d83c42789c7f648cce9c64218f
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-07-09 21:29:02 +0500

    routing practice

--- 3-routing/src/main.jsx
+++ 3-routing/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Profile from "./components/Profile.jsx";
+import DefaultProfile from "./components/DefaultProfile";
+import Spinach from "./components/Spinach.jsx";
+import Popeye from "./components/Popeye.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile /> },
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
